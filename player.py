a56a242931edbea40805f08a260be6f5e6aceada
Edin Kocan <116869401+EdoBro@users.noreply.github.com>
2024-04-06 12:56:08 +0200

    Update player.py

--- player.py
+++ player.py
@@ -4,5 +4,5 @@ from settings import *
 class Player(pygame.sprite.Sprite):
 	def __init__(self,pos,groups):
 		super().__init__(groups)
-		self.image = pygame.image.load('C:\Users\Korisnik\Desktop\1 - level\graphics\test\player.png').convert_alpha()
+		self.image = pygame.image.load("C:/Users/Korisnik/Desktop/Zelda Souls/graphics/test/player.png").convert_alpha()
 		self.rect = self.image.get_rect(topleft = pos)
